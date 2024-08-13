26de2bb33a81d91cf1bb50e34086de6881f7dcef
Hailen <erpit2@deogra.com>
2024-08-13 15:03:04 +0800

    feat:配置启动环境

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
-})
+  build: {
+    outDir: "./dist", // 输出目录
+    assetsDir: "assets", // 静态资源目录
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "./index.html"),
+        preload: resolve(__dirname, "src/Main/preload.ts"),
+        main: resolve(__dirname, "src/Main/index.ts"),
+      },
+      external: ["electron", "fs", "os", "path", "url"],
+      output: {
+        entryFileNames: (chunkInfo) => {
+          if (chunkInfo.name === "preload") {
+            return "preload.cjs"; // 为特殊文件自定义名称
+          }
+          return "[name].js"; // 其他文件使用默认名称
+        },
+        globals: {
+          electron: "electron",
+          fs: "fs",
+          os: "os",
+          path: "path",
+          url: "url",
+        },
+      },
+    },
+  },
+  server: {
+    port: 3000,
+  },
+});
