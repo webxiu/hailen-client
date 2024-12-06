9f9afc27ffab420aeea567c49feb423f687fa356
Hailen <759430324@qq.com>
2024-12-07 00:01:47 +0800

    fix: fix

--- src/Main/server/middlewares/token.ts
+++ src/Main/server/middlewares/token.ts
@@ -4,10 +4,11 @@ import jwt from "jsonwebtoken";
 import { responseStatus } from "../config";
 
 export const verifyToken = async (ctx: any, next: any) => {
-  const token = ctx.request.headers["authorization"];
-  // if (whiteList.includes(ctx.url)) return await next();
-  if (!token) return (ctx.body = responseStatus(401, "Token已过期"));
   try {
+    const token = ctx.request.headers["authorization"];
+    const isPass = whiteList.some((url) => ctx.url.startsWith(url));
+    if (isPass) return await next();
+    if (!token) return (ctx.body = responseStatus(401, "Token已过期"));
     const decoded = jwt.verify(token, secretKey);
     ctx.state.user = decoded; // 将解码后的用户信息存储在 ctx.state 中
     await next(); // 继续执行下一个中间件
