229615300ebeb8bf007e422c1923944bf4e226d9
Hailen <759430324@qq.com>
2025-06-28 16:21:35 +0800

    fix: 钢琴按键布局完善

---
+++ src/vue/views/home/tool/transform/KeyBit.vue
@@ -0,0 +1,83 @@
+<script lang="tsx" setup>
+defineProps<{ item: any }>();
+</script>
+
+<template>
+  <div class="item-key">
+    <div class="top"></div>
+    <div class="front"></div>
+    <div class="left side"></div>
+    <div class="right side"></div>
+    <div class="key-text">{{ item.key }}</div>
+  </div>
+</template>
+
+<style lang="scss">
+.item-key {
+  z-index: 1;
+  width: 60px;
+  height: 70px;
+  position: absolute;
+  backface-visibility: hidden;
+  transform-style: preserve-3d;
+  transform-origin: 0% 0% -400px;
+  transition: 0.2s;
+
+  &.pressed {
+    transform: rotateX(-5deg);
+  }
+
+  .front {
+    width: 60px;
+    height: 70px;
+    position: relative;
+    background: #eee;
+    transform: rotateX(0deg);
+    border: 1px solid #ccc;
+    background-image: linear-gradient(to top bottom, #fff, #eee);
+  }
+
+  .top {
+    z-index: 9;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 60px;
+    height: 400px;
+    background: #fff;
+    border: 1px solid #ccc;
+    transform: rotateX(270deg);
+    transform-origin: 0% 0%;
+    transform-style: preserve-3d;
+    background-image: linear-gradient(to bottom right, #eee, #fff);
+  }
+  .side {
+    width: 400px;
+    height: 70px;
+    top: 0;
+    left: 0;
+    position: absolute;
+    border: 1px solid #ccc;
+    transform: rotateY(90deg);
+    transform-origin: 0% 0%;
+    background-image: linear-gradient(to bottom right, #fff, #999);
+    &.left {
+      left: 0;
+    }
+    &.right {
+      left: 60px;
+    }
+  }
+  .key-text {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 60px;
+    height: 70px;
+    text-align: center;
+    line-height: 70px;
+    font-size: 14px;
+    color: #333;
+  }
+}
+</style>
--- src/vue/views/home/tool/transform/index.vue
+++ src/vue/views/home/tool/transform/index.vue
@@ -1,25 +1,19 @@
 <template>
-  <div class="flex-col" ref="pianoRef">
-    <div v-for="(item, index) in audioButtons" :key="index" class="flex">
-      <div v-for="(m, i) in item" :key="i" @click="onPlay(m)" class="mr-5 piano-key" :class="{ active: m.key === active }">
-        <div>
-          <div>{{ m.note }}</div>
-          <div class="mt-15 color-00f">{{ m.key }}</div>
-        </div>
-      </div>
-    </div>
+  <div class="piano-box">
+    <KeyBit v-for="(item, index) in keysList" :key="item.key" :item="item" :class="{ pressed: pressKeys.includes(item.key) }" :style="{ left: `${index * 60}px` }" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
+import { ref, onMounted, reactive, computed } from "vue";
+import KeyBit from "./KeyBit.vue";
 const audioData = [261.63, 293.66, 329.63, 349.23, 392.0, 440.0, 493.88];
 // ["c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b"],
 // const audio_type_list = ["sine", "square", "sawtooth", "triangle"];
 const keys = [
   {
     type: "sine",
-    rate: 1,
+    rate: 3,
     keys: [
       { key: "Q", note: "c" },
       { key: "W", note: "d" },
@@ -44,31 +38,35 @@ const keys = [
     ]
   }
 ];
-const pianoRef = ref();
-const active = ref("");
+
+const status = ref(1);
+const pressKeyMap = reactive({});
+const pressKeys = computed(() => Object.values(pressKeyMap));
 
 const audioButtons = keys.map((m) => m.keys.map((n, i) => ({ type: m.type, rhythm: i, key: n.key, note: n.note, rate: m.rate })));
+const keysList = audioButtons.flat();
 
 onMounted(() => {
-  window.addEventListener("keydown", onKeyDown);
+  window.addEventListener("keypress", onKeypress);
   window.addEventListener("keyup", onKeyUp);
 });
 
-function onKeyDown(e) {
+function onKeypress(e) {
   const key = e.key.toUpperCase();
-  active.value = key;
-  const item = audioButtons.flat().find((item) => item.key.toLowerCase() === key.toLowerCase());
-  console.log("按下", key, item);
+  if (pressKeyMap[key]) return;
+  pressKeyMap[key] = key;
+  const item = keysList.find((item) => item.key.toLowerCase() === key.toLowerCase());
   if (!item) return;
   onPlay(item);
 }
 
 function onKeyUp(e) {
-  active.value = "";
+  const key = e.key.toUpperCase();
+  pressKeyMap[key] = "";
 }
 
 function onPlay(item) {
-  getAudio(item.type, item.rhythm, item.rate);
+  getAudio(item.type, item.rhythm, status.value);
 }
 
 const getAudio = (type: OscillatorType, index: number, rate: number) => {
@@ -97,62 +95,35 @@ const getAudio = (type: OscillatorType, index: number, rate: number) => {
   // 1秒后停止声音
   oscillator.stop(audioCtx.currentTime + 1);
 };
-
-function setType(i) {
-  index.value = 0;
-  title.value = "开始播放";
-  typeIndex.value = i;
-}
-function create() {
-  title.value = `基础音：${index.value === 7 ? "+1" : index.value + 1}`;
-  // 创建音频上下文
-  const audioCtx = new AudioContext();
-  // 创建音调控制对象
-  const oscillator = audioCtx.createOscillator();
-  // 创建音量控制对象
-  const gainNode = audioCtx.createGain();
-  // 音调音量关联
-  oscillator.connect(gainNode);
-  // 音量和设备关联
-  gainNode.connect(audioCtx.destination);
-  // 音调类型指定为正弦波
-  oscillator.type = audio_type_list[typeIndex.value] as OscillatorType;
-  // 设置音调频率
-  oscillator.frequency.value = audio_data_list[index.value];
-  // 先把当前音量设为0
-  gainNode.gain.setValueAtTime(0, audioCtx.currentTime);
-  // 0.01秒时间内音量从刚刚的0变成1，线性变化
-  gainNode.gain.linearRampToValueAtTime(1, audioCtx.currentTime + 0.01);
-  // 声音走起
-  oscillator.start(audioCtx.currentTime);
-  // 1秒时间内音量从刚刚的1变成0.001，指数变化
-  gainNode.gain.exponentialRampToValueAtTime(0.1, audioCtx.currentTime + 1);
-  // 1秒后停止声音
-  oscillator.stop(audioCtx.currentTime + 1);
-  if (index.value >= audio_data_list.length - 1) {
-    index.value = 0;
-  } else {
-    index.value++;
-  }
-}
 </script>
 
 <style>
 .piano-key {
   width: 60px;
   height: 100px;
-  background-color: white;
-  border: 1px solid #ccc;
+  cursor: pointer;
+  margin-right: 2px;
   display: flex;
   align-items: flex-end;
   justify-content: center;
-  margin-right: 2px;
   transition: all 0.1s ease;
+  border: 1px solid #ccc;
+  background-color: white;
   box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
-  cursor: pointer;
 }
 .piano-key.active {
   background-color: #f0f0f0;
   transform: scale(1, 0.98);
+}
+/* 钢琴 */
+.piano-box {
+  width: 800px;
+  height: 800px;
+  top: 50px;
+  left: 50%;
+  position: relative;
+  perspective: 1500px;
+  transform-style: preserve-3d;
+  transform: translateX(-50%) translateZ(10px) rotateY(0deg) rotateX(-50deg);
 }
 </style>
